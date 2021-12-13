784d16d930d485ccd5234aa38d4ddcc80bec427b
Fernando <nando.calheirosx@gmail.com>
2021-12-13 14:39:59 -0300

    Order repository

---
+++ Bob.Services.OrderAPI/Repositories/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Bob.Services.OrderAPI.Models;
+using System.Threading.Tasks;
+
+namespace Bob.Services.OrderAPI.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}
---
+++ Bob.Services.OrderAPI/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using Bob.Services.OrderAPI.Models;
+using Manage.Services.OrderAPI;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Bob.Services.OrderAPI.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _dbContext;
+
+        public OrderRepository(DbContextOptions<ApplicationDbContext> dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            await using var _db = new ApplicationDbContext(_dbContext);
+            _db.OrderHeader.Add(orderHeader);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            await using var _db = new ApplicationDbContext(_dbContext);
+            var orderHeaderFromDb = await _db.OrderHeader.FirstOrDefaultAsync(u => u.Id == orderHeaderId);
+            if (orderHeaderFromDb != null)
+            {
+                orderHeaderFromDb.PaymentStatus = paid;
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
